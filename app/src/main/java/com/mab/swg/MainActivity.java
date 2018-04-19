aad337a3dd4325bbb58943b74d3ceab566c3a7f4
MohammadAmin <aminbahmani10@gmail.com>
2018-04-19 12:49:20 +0430

    Add SpeedMatchActivity UI WhichOneIsLargerActivity UI

--- app/src/main/java/com/mab/swg/MainActivity.java
+++ app/src/main/java/com/mab/swg/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mab.swg;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +10,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv1,tv2;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv1 = findViewById(R.id.sp_title);
         tv2 = findViewById(R.id.gl_title);
+
+        frameLayout = findViewById(R.id.speed_match_start_fragment);
+        frameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SpeedMatchActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/GILSANUB.TTF");
         tv1.setTypeface(typeface);
---
+++ app/src/main/java/com/mab/swg/WhichOneIsLargerActivity.java
@@ -0,0 +1,13 @@
+package com.mab.swg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhichOneIsLargerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_which_one_is_larger);
+    }
+}
